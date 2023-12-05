20495cf2067d8eaa52c21bd646f11cd5af708f81
Rohith Ravi <rohith.ravi@mixpanel.com>
2023-12-05 13:29:31 -0800

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::path::Path;
+
 use anyhow::Result;
 
 pub fn runner<A: std::fmt::Display, B: std::fmt::Display>(
@@ -5,13 +7,19 @@ pub fn runner<A: std::fmt::Display, B: std::fmt::Display>(
     part_two: fn(&str) -> Result<B>,
 ) -> Result<()> {
     let mut args = std::env::args();
+    let binary_path = args.next().expect("binary name to be present");
+    let binary_name = Path::new(&binary_path)
+        .file_name()
+        .and_then(|p| p.to_str())
+        .expect("file_name to_str failed");
     let cmd = args
-        .nth(1)
-        .expect("usage: cmd [1|2] input_file_path. cmd is missing");
+        .next()
+        .expect("usage: cmd [1|2] [input_file_path]. cmd is missing");
     let input_file_path = args
         .next()
-        .expect("usage: cmd [1|2] input_file_path. input_file_path is missing");
-    let input = std::fs::read_to_string(input_file_path).expect("unable to read input file");
+        .unwrap_or_else(|| format!("inputs/{}.txt", binary_name));
+    let input = std::fs::read_to_string(&input_file_path)
+        .expect(format!("unable to read input file: {input_file_path}").as_str());
     match cmd.as_str() {
         "1" => {
             println!("{}", part_one(&input)?);
