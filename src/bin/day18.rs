606837fc9825265df12a625f9f1c0d4be5e6bfe7
Rohith Ravi <rohith.ravi@mixpanel.com>
2023-12-27 16:14:33 -0800

    part2

--- src/bin/day18.rs
+++ src/bin/day18.rs
@@ -1,41 +1,43 @@
 #![feature(iter_map_windows)]
-#![allow(unused, dead_code)]
 
 use anyhow::Result;
 use aoc::{must_parse, runner};
 use nom::{
     bytes::complete::{is_a, tag},
-    character::complete::{self, hex_digit1, newline, one_of, space1},
-    combinator::{map, map_res},
+    character::complete::{self, newline, one_of, space1},
+    combinator::map,
     multi::separated_list1,
-    number::complete::hex_u32,
-    sequence::{delimited, preceded, tuple},
+    sequence::{delimited, tuple},
 };
 
 fn main() -> Result<()> {
     runner(part_one, part_two)
 }
 
-fn part_one(input: &str) -> Result<u32> {
+fn part_one(input: &str) -> Result<u64> {
     let p = Puzzle::parse(input)?;
-    let coords = p.as_coords();
-    let area = compute_area_via_shoelace(coords.as_slice());
-    let b = num_boundary_points(coords.as_slice());
-    // See: https://en.wikipedia.org/wiki/Pick's_theorem
-    let i = (area + 1) - b / 2;
-    Ok(i + b)
+    Ok(compute_lava_cubes(&p.as_coords()))
 }
 
-fn part_two(_input: &str) -> Result<u32> {
-    todo!()
+fn part_two(input: &str) -> Result<u64> {
+    let p = Puzzle::parse(input)?;
+    Ok(compute_lava_cubes(&p.as_corrected().as_coords()))
+}
+
+fn compute_lava_cubes(coords: &[(i64, i64)]) -> u64 {
+    let area = compute_area_via_shoelace(coords);
+    let b = num_boundary_points(coords);
+    // See: https://en.wikipedia.org/wiki/Pick's_theorem
+    let i = (area + 1) - b / 2;
+    i + b
 }
 
 #[derive(Debug)]
-struct Puzzle {
-    ins: Vec<Instruction>,
+struct Puzzle<'i> {
+    ins: Vec<Instruction<'i>>,
 }
-impl Puzzle {
-    fn parse(input: &str) -> Result<Self> {
+impl<'i> Puzzle<'i> {
+    fn parse(input: &'i str) -> Result<Self> {
         let parse_direction = map(one_of("UDRL"), |ch: char| match ch {
             'U' => Direction::Up,
             'D' => Direction::Down,
@@ -43,26 +45,23 @@ impl Puzzle {
             'R' => Direction::Right,
             other => unreachable!("unexpected direction: {}", other),
         });
-        let parse_hex = map_res(is_a("0123456789abcdef"), |as_str: &str| {
-            u32::from_str_radix(as_str, 16)
-        });
-        let parse_color = delimited(tag("(#"), parse_hex, complete::char(')'));
+        let parse_color = delimited(tag("(#"), is_a("0123456789abcdef"), complete::char(')'));
         let parse_ins = map(
-            tuple((parse_direction, space1, complete::u8, space1, parse_color)),
+            tuple((parse_direction, space1, complete::u64, space1, parse_color)),
             |(dir, _, count, _, color)| Instruction { dir, count, color },
         );
         let parse_instructions = separated_list1(newline, parse_ins);
-        let mut parser = map(parse_instructions, |ins: Vec<Instruction>| Self { ins });
+        let parser = map(parse_instructions, |ins: Vec<Instruction>| Self { ins });
         must_parse(parser, input)
     }
 
-    fn as_coords(&self) -> Vec<(i32, i32)> {
+    fn as_coords(&self) -> Vec<(i64, i64)> {
         let iter = self.ins.iter().scan((0, 0), |(x, y), ins| {
             let (nx, ny) = match ins.dir {
-                Direction::Up => (*x, *y + ins.count as i32),
-                Direction::Down => (*x, *y - ins.count as i32),
-                Direction::Left => (*x - ins.count as i32, *y),
-                Direction::Right => (*x + ins.count as i32, *y),
+                Direction::Up => (*x, *y + ins.count as i64),
+                Direction::Down => (*x, *y - ins.count as i64),
+                Direction::Left => (*x - ins.count as i64, *y),
+                Direction::Right => (*x + ins.count as i64, *y),
             };
             *x = nx;
             *y = ny;
@@ -72,13 +71,36 @@ impl Puzzle {
         ret.extend(iter);
         ret
     }
+
+    fn as_corrected(&self) -> Self {
+        let ins = self
+            .ins
+            .iter()
+            .map(|ins| {
+                let count = u64::from_str_radix(&ins.color[..5], 16).expect("dist parsing failed");
+                let dir = match ins.color.as_bytes()[5] {
+                    b'0' => Direction::Right,
+                    b'1' => Direction::Down,
+                    b'2' => Direction::Left,
+                    b'3' => Direction::Up,
+                    other => unreachable!("unexpected direction: {}", other),
+                };
+                Instruction {
+                    dir,
+                    count,
+                    color: ins.color,
+                }
+            })
+            .collect();
+        Self { ins }
+    }
 }
 
 #[derive(Debug)]
-struct Instruction {
+struct Instruction<'i> {
     dir: Direction,
-    count: u8,
-    color: u32,
+    count: u64,
+    color: &'i str,
 }
 
 #[derive(Debug)]
@@ -90,21 +112,21 @@ enum Direction {
 }
 
 // See: https://en.wikipedia.org/wiki/Shoelace_formula
-fn compute_area_via_shoelace(coords: &[(i32, i32)]) -> u32 {
+fn compute_area_via_shoelace(coords: &[(i64, i64)]) -> u64 {
     assert!(coords.first() == coords.last());
     coords
         .iter()
         .map_windows(|&[(x1, y1), (x2, y2)]| (x1 * y2) - (x2 * y1))
-        .sum::<i32>()
-        .abs() as u32
+        .sum::<i64>()
+        .abs() as u64
         / 2
 }
 
-fn num_boundary_points(points: &[(i32, i32)]) -> u32 {
+fn num_boundary_points(points: &[(i64, i64)]) -> u64 {
     points
         .iter()
         .map_windows(|&[(x1, y1), (x2, y2)]| x1.abs_diff(*x2) + y1.abs_diff(*y2))
-        .sum::<u32>()
+        .sum::<u64>()
 }
 
 #[cfg(test)]
@@ -133,9 +155,15 @@ U 2 (#7a21e3)"#;
     }
 
     #[test]
+    fn test_part_two() -> Result<()> {
+        assert_eq!(part_two(INPUT)?, 952408144115);
+        Ok(())
+    }
+
+    #[test]
     fn test_coords() -> Result<()> {
         let p = Puzzle::parse(INPUT)?;
-        let coords = p.as_coords();
+        let _coords = p.as_coords();
         Ok(())
     }
 }
