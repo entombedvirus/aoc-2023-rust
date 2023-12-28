084aa588212afc6b93dbd30343d66b43b8afbbb9
Rohith Ravi <rohith.ravi@mixpanel.com>
2023-12-28 15:46:38 -0800

    part2

--- src/bin/day19.rs
+++ src/bin/day19.rs
@@ -23,8 +23,26 @@ fn part_one(input: &str) -> Result<u32> {
     Ok(bins["A"].iter().map(|p| p.part_sum()).sum())
 }
 
-fn part_two(_input: &str) -> Result<u32> {
-    todo!()
+fn part_two(input: &str) -> Result<u64> {
+    let p = Puzzle::parse(input)?;
+    let valid_range = 1..4001;
+
+    let mut count = 0;
+    let mut queue = vec![("in", CountState::new(&valid_range))];
+
+    // perform DFS of the graph summing up possibilities every time we reach the "A" terminal node
+    while let Some((name, state)) = queue.pop() {
+        let w = &p.workflows[name];
+        for (child, state) in w.child_with_state(&state) {
+            match child {
+                "A" => count += state.num_possibilities(),
+                "R" => continue,
+                other => queue.push((other, state)),
+            }
+        }
+    }
+
+    Ok(count)
 }
 
 #[derive(Debug)]
@@ -42,19 +60,14 @@ impl<'i> Puzzle<'i> {
             's' => PartField::S,
             unknown => unreachable!("unknown part field: {}", unknown),
         });
-        let parse_op = map(one_of("<>"), |ch| match ch {
-            '<' => Operation::LessThan,
-            '>' => Operation::GreaterThan,
+        let parse_op = map(tuple((one_of("<>"), complete::u16)), |(ch, num)| match ch {
+            '<' => Operation::LessThan(num),
+            '>' => Operation::GreaterThan(num),
             unknown => unreachable!("unknown operation: {}", unknown),
         });
         let parse_cond = map(
-            tuple((
-                parse_part_field,
-                parse_op,
-                complete::u32,
-                complete::char(':'),
-            )),
-            |(field, op, operand, _)| Condition { field, op, operand },
+            tuple((parse_part_field, parse_op, complete::char(':'))),
+            |(field, op, _)| Condition { field, op },
         );
         let parse_step = map(tuple((opt(parse_cond), alpha1)), |(cond, dest_workflow)| {
             Step {
@@ -84,16 +97,16 @@ impl<'i> Puzzle<'i> {
         let parse_part = map(
             tuple((
                 tag("x="),
-                complete::u32,
+                complete::u16,
                 complete::char(','),
                 tag("m="),
-                complete::u32,
+                complete::u16,
                 complete::char(','),
                 tag("a="),
-                complete::u32,
+                complete::u16,
                 complete::char(','),
                 tag("s="),
-                complete::u32,
+                complete::u16,
             )),
             |(_, x, _, _, m, _, _, a, _, _, s)| Part { x, m, a, s },
         );
@@ -119,19 +132,122 @@ impl<'i> Puzzle<'i> {
                 .or_insert(vec![part]);
         }
     }
+
+    fn num_possibilities(&self, valid_range: std::ops::Range<u16>) -> u64 {
+        let mut count = 0;
+
+        let mut queue = vec![("in", CountState::new(&valid_range))];
+        while let Some((name, state)) = queue.pop() {
+            let w = &self.workflows[name];
+            for (child, state) in w.child_with_state(&state) {
+                match child {
+                    "A" => count += state.num_possibilities(),
+                    "R" => continue,
+                    other => queue.push((other, state)),
+                }
+            }
+        }
+
+        count
+    }
+}
+
+#[derive(Debug, Clone)]
+struct CountState {
+    x: SplittableRange,
+    m: SplittableRange,
+    a: SplittableRange,
+    s: SplittableRange,
+}
+
+impl CountState {
+    fn new(valid_range: &std::ops::Range<u16>) -> Self {
+        Self {
+            x: SplittableRange::new(valid_range),
+            m: SplittableRange::new(valid_range),
+            a: SplittableRange::new(valid_range),
+            s: SplittableRange::new(valid_range),
+        }
+    }
+
+    fn num_possibilities(&self) -> u64 {
+        let Self { x, m, a, s } = self;
+        [x, m, a, s]
+            .into_iter()
+            .map(|srange| srange.len() as u64)
+            .product()
+    }
+
+    /// restrict_and_shrink retruns the subset of `self` that matches `cond`. It also shrinks `self` to
+    /// the parts that are not covered by `cond`.
+    fn restrict_and_shrink(&mut self, cond: &Condition) -> CountState {
+        let mut restricted = self.clone();
+        let (self_range, restricted_range) = match cond.field {
+            PartField::X => (&mut self.x, &mut restricted.x),
+            PartField::M => (&mut self.m, &mut restricted.m),
+            PartField::A => (&mut self.a, &mut restricted.a),
+            PartField::S => (&mut self.s, &mut restricted.s),
+        };
+        restricted_range.apply(&cond.op);
+        self_range.apply(&cond.op.negate());
+
+        restricted
+    }
+}
+
+#[derive(Debug, Clone)]
+struct SplittableRange {
+    points: BTreeMap<u16, u16>,
+}
+
+impl SplittableRange {
+    fn new(valid_range: &std::ops::Range<u16>) -> SplittableRange {
+        let mut points = BTreeMap::new();
+        points.insert(valid_range.start, valid_range.end);
+        Self { points }
+    }
+
+    fn len(&self) -> u16 {
+        self.points.iter().map(|(start, end)| *end - *start).sum()
+    }
+
+    fn apply(&mut self, op: &Operation) {
+        match *op {
+            Operation::LessThan(x) => {
+                self.points.retain(|&start, _| start < x);
+                if let Some(entry) = self.points.last_entry() {
+                    if (*entry.get()) > x {
+                        let closed_start = *entry.key();
+                        entry.remove();
+                        self.points.insert(closed_start, x);
+                    }
+                }
+            }
+            Operation::GreaterThan(x) => {
+                self.points.retain(|_, end| *end > x);
+                if let Some(entry) = self.points.first_entry() {
+                    if (*entry.key()) <= x {
+                        let open_end = *entry.get();
+                        entry.remove();
+                        self.points.insert(x + 1, open_end);
+                    }
+                }
+            }
+        }
+    }
 }
 
 #[derive(Debug)]
 struct Part {
-    x: u32,
-    m: u32,
-    a: u32,
-    s: u32,
+    x: u16,
+    m: u16,
+    a: u16,
+    s: u16,
 }
 impl Part {
     fn part_sum(&self) -> u32 {
         let &Self { x, m, a, s } = self;
-        x + m + a + s
+        [x, m, a, s].into_iter().map(|x| x as u32).sum()
     }
 }
 
@@ -140,12 +256,27 @@ struct Workflow<'i> {
     name: &'i str,
     steps: Vec<Step<'i>>,
 }
-impl Workflow<'_> {
+impl<'i> Workflow<'i> {
     fn run(&self, part: &Part) -> &str {
         self.steps
             .iter()
             .find_map(|step| step.run(part))
             .expect("last step is always expected to match")
+    }
+
+    fn child_with_state(
+        &'i self,
+        input: &CountState,
+    ) -> impl Iterator<Item = (&'i str, CountState)> + 'i {
+        self.steps
+            .iter()
+            .scan(input.clone(), |state, st| match &st.cond {
+                Some(cond) => {
+                    let child_state = state.restrict_and_shrink(cond);
+                    Some((st.dest_workflow, child_state))
+                }
+                None => Some((st.dest_workflow, state.clone())),
+            })
     }
 }
 
@@ -168,17 +299,17 @@ impl Step<'_> {
 struct Condition {
     field: PartField,
     op: Operation,
-    operand: u32,
 }
+
 impl Condition {
     fn eval(&self, part: &Part) -> bool {
-        let op1 = match self.field {
+        let op = match self.field {
             PartField::X => part.x,
             PartField::M => part.m,
             PartField::A => part.a,
             PartField::S => part.s,
         };
-        self.op.eval(op1, self.operand)
+        self.op.eval(op)
     }
 }
 
@@ -192,14 +323,22 @@ enum PartField {
 
 #[derive(Debug)]
 enum Operation {
-    LessThan,
-    GreaterThan,
+    LessThan(u16),
+    GreaterThan(u16),
 }
+
 impl Operation {
-    fn eval(&self, op1: u32, op2: u32) -> bool {
+    fn eval(&self, op1: u16) -> bool {
         match self {
-            Operation::LessThan => op1 < op2,
-            Operation::GreaterThan => op1 > op2,
+            Operation::LessThan(op2) => op1 < *op2,
+            Operation::GreaterThan(op2) => op1 > *op2,
+        }
+    }
+
+    fn negate(&self) -> Self {
+        match self {
+            Operation::LessThan(x) => Operation::GreaterThan(*x - 1),
+            Operation::GreaterThan(x) => Operation::LessThan(*x + 1),
         }
     }
 }
@@ -229,6 +368,12 @@ hdj{m>838:A,pv}
     #[test]
     fn test_part_one() -> Result<()> {
         assert_eq!(part_one(INPUT)?, 19114);
+        Ok(())
+    }
+
+    #[test]
+    fn test_part_two() -> Result<()> {
+        assert_eq!(part_two(INPUT)?, 167409079868000);
         Ok(())
     }
 }
