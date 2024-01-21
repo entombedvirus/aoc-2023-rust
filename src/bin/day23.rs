febcb014f7b6d24938ca6d8e4c000456261c91c7
Rohith Ravi <rohith.ravi@mixpanel.com>
2024-01-20 21:50:51 -0800

    part 2

--- src/bin/day23.rs
+++ src/bin/day23.rs
@@ -1,9 +1,9 @@
 #![allow(unused, dead_code)]
-
-use std::collections::{BTreeMap, VecDeque};
+#![feature(extract_if)]
+use std::collections::{BTreeMap, BTreeSet, VecDeque};
 
 use anyhow::Result;
-use aoc::{must_parse, runner};
+use aoc::{must_parse, runner, wait};
 use nom::{character::complete::newline, multi::separated_list1};
 
 fn main() -> Result<()> {
@@ -15,30 +15,88 @@ fn part_one(input: &str) -> Result<u32> {
     let start = p.start_pos().ok_or(anyhow::format_err!("no start pos"))?;
     let finish = p.finish_pos().ok_or(anyhow::format_err!("no finish pos"))?;
 
-    let mut longest_hike = None;
-    let mut q = VecDeque::new();
-    q.push_back(Head::new(start, 0));
-    while let Some(mut head) = q.pop_front() {
-        if head.cur_pos == finish {
-            longest_hike = std::cmp::max(longest_hike, head.cost());
-        }
-        let mut neighbors = p.get_neighbor_pos(&head);
-        while let Some(npos) = neighbors.pop() {
-            let mut head = if neighbors.is_empty() {
-                std::mem::take(&mut head)
-            } else {
-                head.clone()
-            };
-            head.move_to(&npos);
-            q.push_back(head);
+    p.longest_path(start, finish)
+        .ok_or(anyhow::format_err!("path to finish not found"))
+}
+
+fn part_two(input: &str) -> Result<u32> {
+    let mut p = Puzzle::parse(input)?;
+    // treat all slides as normal tiles
+    for tile in &mut p.tiles {
+        match *tile {
+            b'>' | b'<' | b'^' | b'v' => *tile = b'.',
+            _ => (),
         }
     }
 
-    longest_hike.ok_or(anyhow::format_err!("path to finish not found"))
+    let start = p.start_pos().ok_or(anyhow::format_err!("no start pos"))?;
+    let finish = p.finish_pos().ok_or(anyhow::format_err!("no finish pos"))?;
+    let graph = p.as_graph(start);
+    eprintln!("{graph}");
+
+    let mut longest_path = None;
+    let mut q = VecDeque::new();
+    q.push_back((start, BTreeSet::new(), String::new(), 0u32));
+
+    while let Some((cur_node, mut seen, mut path, cost)) = q.pop_back() {
+        seen.insert(cur_node);
+        if !path.is_empty() {
+            path.push_str(format!(" -{cost}-> ").as_str())
+        }
+        path.push_str(format!("{cur_node:?}").as_str());
+        if cur_node == finish {
+            longest_path = std::cmp::max(longest_path, Some(cost));
+            continue;
+        }
+        for (neighbor, ncost) in graph.edges_from(cur_node) {
+            if !seen.contains(&neighbor) {
+                q.push_back((neighbor, seen.clone(), path.clone(), cost + ncost));
+            }
+        }
+    }
+    longest_path.ok_or(anyhow::format_err!("path to finish not found"))
 }
 
-fn part_two(_input: &str) -> Result<u32> {
-    todo!()
+type Pos = (isize, isize);
+
+#[derive(Debug, Default)]
+struct Graph {
+    neighbors: BTreeMap<Pos, BTreeSet<Pos>>,
+    edges: BTreeMap<(Pos, Pos), u32>,
+}
+
+impl Graph {
+    fn add_edge(&mut self, n1: Pos, n2: Pos, cost: u32) {
+        self.edges.entry((n1, n2)).or_insert(cost);
+        self.edges.entry((n2, n1)).or_insert(cost);
+        self.neighbors.entry(n1).or_default().insert(n2);
+        self.neighbors.entry(n2).or_default().insert(n1);
+    }
+
+    fn edges_from<'a>(&'a self, cur_node: (isize, isize)) -> impl Iterator<Item = (Pos, u32)> + 'a {
+        self.neighbors[&cur_node]
+            .iter()
+            .map(move |neighbor| (*neighbor, self.edges[&(cur_node, *neighbor)]))
+        // self.edges
+        //     .iter()
+        //     .skip_while(move |((from, _), _)| *from != cur_node)
+        //     .take_while(move |((from, _), _)| *from == cur_node)
+        //     .map(|((_from, to), cost)| (*to, *cost))
+    }
+}
+
+impl std::fmt::Display for Graph {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        writeln!(f, "Graph:")?;
+        for ((from, to), cost) in &self.edges {
+            writeln!(
+                f,
+                "\t{from:?} --{cost}--> {to:?}, neighbors: {:?}",
+                self.neighbors.get(from),
+            )?;
+        }
+        Ok(())
+    }
 }
 
 #[derive(Debug, Clone, Default)]
@@ -54,8 +112,8 @@ impl Head {
         Self { seen, cur_pos }
     }
 
-    fn move_to(&mut self, pos: &(isize, isize)) {
-        let cost = self.seen[&self.cur_pos] + 1;
+    fn move_to(&mut self, pos: &(isize, isize), cost_delta: u32) {
+        let cost = self.seen[&self.cur_pos] + cost_delta;
         self.seen.entry(*pos).or_insert(cost);
         self.cur_pos = *pos;
     }
@@ -130,17 +188,14 @@ impl Puzzle {
         }
     }
 
-    fn get_neighbor_pos(&self, head: &Head) -> Vec<(isize, isize)> {
+    fn get_neighbor_pos(&self, cur_pos: (isize, isize)) -> Vec<(isize, isize)> {
         let mut npos = Vec::new();
         let mut push_if_valid = |pos| {
             match self.get_tile(pos) {
-                Some(b'.' | b'<' | b'>' | b'^' | b'v') if !head.seen.contains_key(&pos) => {
-                    npos.push(pos)
-                }
+                Some(b'.' | b'<' | b'>' | b'^' | b'v') => npos.push(pos),
                 _ => (),
             };
         };
-        let cur_pos = head.cur_pos;
         match self.get_tile(cur_pos) {
             Some(b'>') => push_if_valid((cur_pos.0, cur_pos.1 + 1)),
             Some(b'<') => push_if_valid((cur_pos.0, cur_pos.1 - 1)),
@@ -155,6 +210,100 @@ impl Puzzle {
         };
         npos
     }
+
+    fn longest_path(&self, start: (isize, isize), finish: (isize, isize)) -> Option<u32> {
+        let mut longest_hike = None;
+        let mut q = VecDeque::new();
+        q.push_back(Head::new(start, 0));
+
+        while let Some(mut head) = q.pop_front() {
+            if head.cur_pos == finish {
+                longest_hike = std::cmp::max(longest_hike, head.cost());
+                continue;
+            }
+            let mut neighbors = self.get_neighbor_pos(head.cur_pos);
+            while let Some(npos) = neighbors.pop() {
+                if head.seen.contains_key(&npos) {
+                    continue;
+                }
+                let mut head = if neighbors.is_empty() {
+                    std::mem::take(&mut head)
+                } else {
+                    head.clone()
+                };
+                head.move_to(&npos, 1);
+                q.push_back(head);
+            }
+        }
+
+        longest_hike
+    }
+
+    fn as_graph(&self, start: (isize, isize)) -> Graph {
+        let mut graph = Graph::default();
+        let mut q = Vec::new();
+        q.push((start, (start.0 + 1, start.1)));
+
+        let mut seen = BTreeSet::new();
+        seen.insert(start);
+
+        while let Some((segment_start_pos, mut cur_pos)) = q.pop() {
+            let mut cost = 0u32;
+            loop {
+                seen.insert(cur_pos);
+                cost += 1;
+                let mut neighbor_positions = self.get_neighbor_pos(cur_pos);
+                neighbor_positions.retain(|pos| !seen.contains(pos));
+                match &neighbor_positions[..] {
+                    [] => {
+                        if cost > 1 {
+                            graph.add_edge(segment_start_pos, cur_pos, cost);
+                        }
+                        break;
+                    }
+                    [npos] => {
+                        if let Some((pending_start_pos, _)) = q.iter().find(|(_, ps)| ps == npos) {
+                            graph.add_edge(segment_start_pos, *pending_start_pos, cost + 2);
+                            break;
+                        }
+                        cur_pos = *npos;
+                        continue;
+                    }
+                    many_npos => {
+                        // cost += 1;
+                        graph.add_edge(segment_start_pos, cur_pos, cost);
+                        q.extend(many_npos.iter().copied().map(|npos| (cur_pos, npos)));
+                        break;
+                    }
+                }
+            }
+        }
+
+        graph
+    }
+
+    // fn debug_print(
+    //     &self,
+    //     q: &[(Node, (isize, isize))],
+    //     node: &Node,
+    //     cur_pos: (isize, isize),
+    //     seen: &BTreeSet<(isize, isize)>,
+    // ) {
+    //     for r in 0..self.num_rows {
+    //         for c in 0..self.num_cols {
+    //             let pos = (r as isize, c as isize);
+    //             let ch = if q.iter().find(|(n, _)| n.pos == pos).is_some() {
+    //                 'P'
+    //             } else if seen.contains(&pos) {
+    //                 'x'
+    //             } else {
+    //                 char::from(self.tiles[r * self.num_cols + c])
+    //             };
+    //             eprint!("{ch}");
+    //         }
+    //         eprintln!();
+    //     }
+    // }
 }
 
 #[cfg(test)]
@@ -188,6 +337,12 @@ mod tests {
     #[test]
     fn test_part_one() -> Result<()> {
         assert_eq!(part_one(INPUT)?, 94);
+        Ok(())
+    }
+
+    #[test]
+    fn test_part_two() -> Result<()> {
+        assert_eq!(part_two(INPUT)?, 154);
         Ok(())
     }
 }
